1980d1776dec71368ea143b28fc7e80f935f15d7
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-10 23:11:28 +0500

    Fix import issue

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import favoritesReducer from "../store/favoritesSlice";
 import cartReducer from "../store/CartSlice";
-
+s
 
 const store = configureStore({
     reducer: {
