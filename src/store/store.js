49aaa7adb26d55c7646f0fbba76d669277aa0261
Azizullokh <azizullokhabdumannopov@gmail.com>
2025-03-10 23:31:22 +0500

    fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import favoritesReducer from "./FavoritesSlice";
 import cartReducer from "../store/CartSlice";
-s
+
 
 const store = configureStore({
     reducer: {
